e0336bbe4e654575579c93ca892857f53229736c
Alec Levin <arl505@humboldt.edu>
2020-09-06 03:10:56 -0400

    git messing with me

--- frontend/src/splash/JoinSessionModal.js
+++ frontend/src/splash/JoinSessionModal.js
@@ -3,16 +3,45 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class JoinSessionModal extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionUrl: "",
+    }
+    this.captureChange = this.captureChange.bind(this);
+    this.redirectToSession = this.redirectToSession.bind(this);
+  }
+
+  redirectToSession() {
+    let sessionGuid = this.getSessionGuidFromUrlOrReturnNullIfInvalid(this.state.sessionUrl);
+    if(sessionGuid !== null) {
+      window.location = process.env.REACT_APP_FRONTEND_BASEURL + '/session/' + sessionGuid;
+    }
+  }
+
+  getSessionGuidFromUrlOrReturnNullIfInvalid(sessionUrl) {
+    let matchedUrl = sessionUrl.match(process.env.REACT_APP_SESSION_REGEX);
+    return matchedUrl === null
+      ? null
+      : sessionUrl.match("[0-9, a-f]{8}-[0-9, a-f]{4}-[0-9, a-f]{4}-[0-9, a-f]{4}-[0-9, a-f]{12}");
+  }
+
+  captureChange(event) {
+    this.setState({sessionUrl: event.target.value});
+  }
+
   render() {
     return (
       <div>
         <Modal backdrop="static" isOpen={this.props.isOpen} toggle={this.props.toggle}>
           <ModalHeader toggle={this.props.toggle}>Join a Lean Coffree session</ModalHeader>
           <ModalBody>
-            Here is where I will ask for details on the session to join: link or id
+            Session link:
+            <br/> 
+            <input type="text" onChange={this.captureChange} placeholder="https://leanCoffree.com/session/67caf957-d01a-4bf2-85db-a4d4bb0fb80e"></input>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.props.toggle}>Join lean coffee session</Button>
+            <Button color="primary" onClick={this.redirectToSession}>Join lean coffee session</Button>
             <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
