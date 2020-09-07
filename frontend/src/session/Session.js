31ef8e8bb1cb52cd18458be3bb384b14db71440e
Alec Levin <arl505@humboldt.edu>
2020-09-06 23:22:56 -0400

    load verification response into state

--- frontend/src/session/Session.js
+++ frontend/src/session/Session.js
@@ -6,7 +6,13 @@ class Session extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isSessionValid: true,
+      isSessionVerified: false,
+      session: {
+        metadata: {
+          id: "",
+          status: ""
+        }
+      }
     }
     this.componentDidMount = this.componentDidMount.bind(this);
   }
@@ -22,20 +28,37 @@ class Session extends React.Component {
       return this.invalidSessionProvided();
     }
 
-    let sessionId = url[0].match("[0-9, a-f]{8}-[0-9, a-f]{4}-[0-9, a-f]{4}-[0-9, a-f]{4}-[0-9, a-f]{12}");
-    if(sessionId === null) {
+    let sessionIdFromAddress = url[0].match("[0-9, a-f]{8}-[0-9, a-f]{4}-[0-9, a-f]{4}-[0-9, a-f]{4}-[0-9, a-f]{12}");
+    if(sessionIdFromAddress === null) {
       return this.invalidSessionProvided();
     }
 
     var self = this;  
-    Axios.post(process.env.REACT_APP_BACKEND_BASEURL + '/verify-session/' + sessionId, null)
+    Axios.post(process.env.REACT_APP_BACKEND_BASEURL + '/verify-session/' + sessionIdFromAddress, null)
       .then(function (response) {
-        self.setState({newSessionId: response.data.toString()});
+        if(self.isVerificationResponseValid(response, sessionIdFromAddress[0])) {
+          self.setState({
+            isSessionVerified: true,
+            session: {
+              metadata: {
+                id: response.data.sessionDetails.sessionId,
+                status: response.data.sessionDetails.sessionStatus,
+              }
+            }
+          })
+        } else {
+          this.invalidSessionProvided();
+        }
       })
       .catch(function (error) {
         console.log("Received an error while verifying session: " + error);
         return self.invalidSessionProvided();
       });
+  }
+
+  isVerificationResponseValid(response, sessionIdFromAddress) {
+    return response.data.verificationStatus === "VERIFICATION_SUCCESS" && response.data.sessionDetails.sessionId === sessionIdFromAddress &&
+    response.data.sessionDetails.sessionStatus === "STARTED";
   }
 
   render() {
