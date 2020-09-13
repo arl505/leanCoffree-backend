1c9d62f6a4bf904404a9eb207854533d692476e8
Alec Levin <arl505@humboldt.edu>
2020-09-13 00:39:48 -0400

    improve validation

--- backend/src/main/java/com/leancoffree/backend/domain/model/SubmitTopicRequest.java
+++ backend/src/main/java/com/leancoffree/backend/domain/model/SubmitTopicRequest.java
@@ -1,6 +1,7 @@
 package com.leancoffree.backend.domain.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SubmitTopicRequest {
 
+  @Size(min = 1, max = 100, message = "sessionId should be between 1-100 chars")
   @NotBlank(message = "sessionId should not be null")
   private String sessionId;
 
+  @Size(min = 1, max = 500, message = "submissionText should be between 1-500 chars")
   @NotBlank(message = "submissionText should not be null")
   private String submissionText;
 }
