46c8c905df61ab68c0cfff759b436ed0321bba45
Alec Levin <alec@arlevin.org>
2020-10-25 22:17:36 -0400

    quick fix for moderator not being broadcasted after any user drops from session (#33)

--- backend/src/main/java/com/leancoffree/backend/service/RefreshUsersService.java
+++ backend/src/main/java/com/leancoffree/backend/service/RefreshUsersService.java
@@ -1,7 +1,6 @@
 package com.leancoffree.backend.service;
 
 import com.leancoffree.backend.domain.model.RefreshUsersRequest;
-import com.leancoffree.backend.domain.model.SessionStatusResponse;
 import com.leancoffree.backend.domain.model.SuccessOrFailureAndErrorBody;
 
 public interface RefreshUsersService {
