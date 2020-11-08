9800ab82ae2b06f49a525730b89c23c7cc6d017c
Alec Levin <arl505@humboldt.edu>
2020-11-08 09:50:59 -0500

    refactor AllUsersList.js

--- frontend/src/session/AllUsersList.js
+++ frontend/src/session/AllUsersList.js
@@ -6,29 +6,22 @@ class AllUsersList extends React.Component {
   render() {
     let allHereListItems = [];
     if(this.props.usersInAttendance.displayNames !== undefined) {
+
       for(let i = 0; i < this.props.usersInAttendance.displayNames.length; i++) {
         let username = this.props.usersInAttendance.displayNames[i];
-        if(this.props.userDisplayName === username) {
-          if(this.props.usersInAttendance.moderator.includes(username)) {
-            allHereListItems.push(<div style={{marginTop: '1vw'}}>
-              <img alt="moderator crown icon" src={moderatorIcon} style={{width: "1em", verticalAlign: 'top'}}></img>
-              <text> </text>
-              <li key={i.toString()} style={{display: 'inline-block'}} class="usernameList"><b>{username}</b></li>
-            </div>);
-          } else {
-            allHereListItems.push(<li key={i.toString()} class="usernameList"><b>{username}</b></li>);
-          }
-        } else {
-          if(this.props.usersInAttendance.moderator.includes(username)) {
-            allHereListItems.push(<div style={{marginTop: '1vw'}}>
-              <img alt="moderator crown icon" src={moderatorIcon} style={{width: "1em", verticalAlign: 'top'}}></img>
-              <text> </text>
-              <li key={i.toString()} style={{display: 'inline-block'}} class="usernameList">{username}</li>
-            </div>);
-          } else {
-            allHereListItems.push(<li key={i.toString()} class="usernameList">{username}</li>);
-          }
-        }
+        let usernameElement = this.props.userDisplayName === username
+          ? <b>{username}</b>
+          : username;
+        let crownVisibility = this.props.usersInAttendance.moderator.includes(username) 
+          ? 'visible'
+          : 'hidden'
+
+        allHereListItems.push(
+          <div style={this.props.usersInAttendance.moderator.includes(username)  ? {marginTop: '1vw'} : {}}>
+            <img alt="moderator crown icon" src={moderatorIcon} style={{width: "1em", verticalAlign: 'top', visibility: crownVisibility}}></img>
+            <text> </text>
+            <li key={i.toString()} style={{display: 'inline-block'}} class="usernameList">{usernameElement}</li>
+          </div>);
       } 
       allHereListItems.push(<div style={{width: '10vw', marginLeft: '.5vw', marginRight:'2.5vw', textAlign: "center"}}><button class="button" style={{marginTop: "1em"}} onClick={this.props.toggleShareableLink}>Invite more</button></div>);
     }
